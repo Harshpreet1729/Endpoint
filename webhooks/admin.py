c7d22d36818603fa2a8371dd0e81bd1e6937f730
HARSHPREET7160 <harshp4466@gmail.com>
2026-08-18 23:25:05 +0530

    Register Event model in Django admin

--- webhooks/admin.py
+++ webhooks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Endpoint
+from .models import Endpoint, Event
 # Register your models here.
 admin.site.register(Endpoint)
+admin.site.register(Event)
 
