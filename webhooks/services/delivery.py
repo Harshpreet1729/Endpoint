531e7b4b2c28f54e82319c97ae090e5508c3af54
HARSHPREET7160 <harshp4466@gmail.com>
2026-08-19 03:10:26 +0530

    Add maximum retry limit

--- webhooks/services/delivery.py
+++ webhooks/services/delivery.py
@@ -1,6 +1,8 @@
 import requests
 from webhooks.models import Delivery
 
+MAX_ATTEMPTS = 5
+
 def deliver_event(event, endpoint):
     try:
         response=requests.post(
@@ -27,4 +29,30 @@ def deliver_event(event, endpoint):
         )
         return None
 
-        
+def retry_delivery(delivery):
+
+    if delivery.attempt_count>=MAX_ATTEMPTS:
+        return None
+    
+    try:
+        response=requests.post(
+                delivery.endpoint.url,
+                json=delivery.event.payload,
+                timeout=5
+        )
+        delivery.attempt_count += 1
+        delivery.status=(
+            Delivery.Status.SUCCESS if response.ok else Delivery.Status.FAILED
+        )
+        delivery.response_status_code=response.status_code
+        delivery.response_body=response.text
+        delivery.save()
+        return response
+    except requests.exceptions.RequestException as e:
+        delivery.attempt_count += 1
+        delivery.status = Delivery.Status.FAILED
+        delivery.response_status_code = None
+        delivery.response_body = str(e)
+        delivery.save()
+        return None
+    
