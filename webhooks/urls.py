01ac93bebf6c25623decbf5d7ee754852821c954
HARSHPREET7160 <harshp4466@gmail.com>
2026-08-20 17:35:17 +0530

    Add Tailwind dashboard with delivery stats

--- webhooks/urls.py
+++ webhooks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import EndpointListClassView, EventListClassView
+from .views import EndpointListClassView, EventListClassView, dashboard
+
 
 urlpatterns = [
     path("endpoints/", EndpointListClassView.as_view(), name="endpoint-list-create"),
     path("events/", EventListClassView.as_view(),name="event-list-create"),
+    path("dashboard/", dashboard, name="dashboard"),
 ]
--- webhooks/views.py
+++ webhooks/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
-from .models import Endpoint, Event
+from django.shortcuts import render
+from .models import Endpoint, Event, Delivery
 from .serializers import EndpointSerializers, EventSerializers
 from rest_framework.permissions import IsAuthenticated
 from .tasks import deliver_event_task
+from django.contrib.auth.decorators import login_required
 
 class EndpointListClassView(generics.ListCreateAPIView):
     serializer_class=EndpointSerializers
@@ -30,3 +32,29 @@ class EventListClassView(generics.ListCreateAPIView):
         )
         for endpoint in endpoints:
             deliver_event_task.delay(event.id, endpoint.id)
+
+@login_required
+def dashboard(request):
+    endpoints=Endpoint.objects.filter(owner=request.user)
+    events=Event.objects.filter(owner=request.user)
+    deliveries=Delivery.objects.filter(
+        event__owner=request.user
+    )
+    context = {
+        "endpoint_count": endpoints.count(),
+        "event_count": events.count(),
+        "success_count": deliveries.filter(
+            status=Delivery.Status.SUCCESS
+        ).count(),
+        "failed_count": deliveries.filter(
+            status=Delivery.Status.FAILED
+        ).count(),
+        "dead_letter_count": deliveries.filter(
+            status=Delivery.Status.DEAD_LETTERED
+        ).count(),
+    }
+    return render(
+        request,
+        "webhooks/dashboard.html",
+        context
+    )
